d902500e800725a431304cbacfdfc452167af403
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-18 21:51:13 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+interface Config {
+  sizes: string[];
+  divider: number;
+}
+const defaultConfig: Config = {
+  sizes: ["Bytes", "KB", "MB", "GB", "TB"],
+  divider: 1000
+};
+
+function getHumanReadableValue(input: number, config: Config = defaultConfig) {
+  if (input == 0) return "0 Byte";
+  const i = Math.floor(Math.log(input) / Math.log(config.divider));
+  const value = Math.round(input / Math.pow(config.divider, i));
+  const size = config.sizes[i];
+  return {
+    value,
+    size
+  };
+}
